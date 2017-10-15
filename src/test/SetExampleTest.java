8970fa609ebbceffa7c00fd235c541e61829a199
virksaabnavjot <virksaabnavjot@gmail.com>
2017-10-16 00:39:46 +0100

    More Unit Testing

--- src/test/SetExampleTest.java
+++ src/test/SetExampleTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
@@ -10,6 +9,8 @@ import static nsv.custom.utilities.Print.*;
 
 class SetExampleTest {
 	//JUnit 4 vs Junit 5 | https://howtoprogram.xyz/2016/08/10/junit-5-vs-junit-4/
+	//JUnit 5 Assertions Example | https://howtoprogram.xyz/2016/08/12/junit-5-assertions-example/
+	
 	@Test
 	void testSize() {
 		SetExample s = new SetExample();
@@ -39,6 +40,32 @@ class SetExampleTest {
 		assertTrue(names.contains("Navjot Singh"));
 		//Asserts that a condition is false.
 		assertFalse(names.contains("Cat"));
+	}
+	
+	@Test
+	void testRemove() {
+		SetExample s = new SetExample();
+		Set <String> names = new HashSet<>();
+		names = s.addValuesTo(names);
+		
+		//testing if the following names exists in the set
+		assertTrue(names.contains("Navjot Singh"));
+		assertTrue(names.contains("Jashan Deep"));
+		
+		//remove the names from the set
+		names.remove("Navjot Singh");
+		names.remove("Jashan Deep");
+		
+		//test/check if the names were removed or still exist in the set
+		assertTrue(!names.contains("Navjot Singh"));
+		assertTrue(!names.contains("Jashan Deep"));
+		
+		//assertAll() example
+		assertAll("Do many assertions.", () -> {
+		      assertNotNull(1);
+		      assertEquals(1, 1);
+		    });
+		
 	}
 
 }
