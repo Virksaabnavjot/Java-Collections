5039c1614dca70cc0cd986b9d1b4034a017fe007
virksaabnavjot <virksaabnavjot@gmail.com>
2017-10-15 22:48:27 +0100

    Sets Example - 3 Types: HashSet(unordered), LinkedHashSet(Ordered), TreeSet(Sorted)

---
+++ src/com/virk/singh/navjot/SetExample.java
@@ -0,0 +1,58 @@
+package com.virk.singh.navjot;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static nsv.custom.utilities.Print.*;
+
+public class SetExample {
+
+	// HashSet | Use where order and sorting is not important
+	private static Set<String> names = new HashSet<>();
+	private static Set<String> namesInOrder = new LinkedHashSet<>();
+	private static Set<String> sortedNames = new TreeSet<>();
+
+	public static void main(String[] args) {
+
+		// adding values to the set (HashSet) and printing the values
+		names = addValuesTo(names);
+		prettyPrint("HashSet Results", names.toString());
+
+		// adding the values to LinkedHashSet (this set remembers the order) and
+		// printing
+		namesInOrder = addValuesTo(namesInOrder);
+		prettyPrint("LinkedHashSet Results", namesInOrder.toString());
+
+		// adding the values to TreeSet(this set sorts the elements) and printing
+		sortedNames = addValuesTo(sortedNames);
+		prettyPrint("TreeSet Results", sortedNames.toString());
+	}
+
+	/*
+	 * this function receives the variable name of the set, and allows us to add
+	 * values to the set according to its type - HashSet, LinkedHashSet, or TreeSet
+	 * and removes the need to repeat code in order to demonstrate all types of sets
+	 * with the same input values
+	 */
+	public static Set<String> addValuesTo(Set<String> name) {
+		name.add("Navjot Singh");
+		name.add("Jashan Deep");
+		name.add("Ranbir Singh");
+		name.add("Amrit Singh");
+		name.add("Baljinder Kaur");
+		name.add("Kuldeep Virk");
+		println("No. of Elements: "+name.size());
+		
+		//duplicate values (In sets duplicates are ignored)
+		
+		name.add("Ranbir Singh");
+		name.add("Amrit Singh");
+		name.add("Baljinder Kaur");
+		name.add("Kuldeep Virk");
+		println("No. of Elements after adding duplicates: "+name.size());
+		
+		return name;
+	}
+}
--- src/nsv/custom/utilities/Print.java
+++ src/nsv/custom/utilities/Print.java
@@ -7,4 +7,14 @@ public class Print {
 	public static void println(String message) {
 		System.out.println(message);
 	}
+	
+	/*this method accepts 2 parameters - 
+	 *1- Title: meaningful you would like to provide to differentiate the results in console
+	 *2- Message: the actual result/message you would like to print
+	 */
+	public static void prettyPrint(String title, String message) {
+		System.out.println("------------ "+title+" ------------");
+		System.out.println(message);
+		System.out.println("------------ ------------ ------------");
+	}
 }
