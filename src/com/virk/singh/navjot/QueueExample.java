4ca56ad1af24523d09ccfa616bee9adddb6c3168
virksaabnavjot <virksaabnavjot@gmail.com>
2017-10-22 00:11:55 +0100

    Test passing after adding elements to the queue - since the queue is not empty anymore with elements in it

--- src/com/virk/singh/navjot/QueueExample.java
+++ src/com/virk/singh/navjot/QueueExample.java
@@ -18,22 +18,26 @@ public class QueueExample {
 	}
 
 	public static void main(String[] args) {
-
-		//Some Manual Testing
-		println(productsQueue);
-		println(productsQueue.poll()); // retrieves and removes the head of the queue
-		println(productsQueue.poll());
-		println(productsQueue); // printing the queue again
 		
 		/*Now, let do something better - Automation Testing using 
 		 * JUnit 5
 		 * Package: test
-		 * 
+		 * File: QueueExampleTests.java
 		 */
         
+		//Some Manual Testing
+		addProductsToTheQueue();
+		println(productsQueue);
+		println(productsQueue.poll()); // retrieves and removes the head of the queue
+		println(productsQueue.poll());
+		println(productsQueue); // printing the queue again
 	}
 
 	public static void addProductsToTheQueue() {
-		
+		productsQueue.add(new Product("Iphone 5s", 125)); // name and weight
+		productsQueue.add(new Product("Protein Shaker", 349));
+		productsQueue.add(new Product("Milk Chocolate", 200));
+		productsQueue.add(new Product("Sweet Candy", 19));
+		productsQueue.add(new Product("Pink Milk Cup", 99));
 	}
 }
